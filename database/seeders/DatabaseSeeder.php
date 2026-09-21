7851a7d3c139bea6b59adf4efaad95ada68cac0c
akidpeyud <dwiputdika@gmail.com>
2026-09-21 09:14:16 +0700

    Update Seeder untuk tabel user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,9 +10,9 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-{
-    $this->call([
-        UsersTableSeeder::class, // (Atau UserSeeder::class jika pakai cara manual)
-    ]);
-}
+    {
+        $this->call([
+            UsersTableSeeder::class,
+        ]);
+    }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,29 +3,100 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // <-- Baris ini sangat penting agar tidak error
 
 class UsersTableSeeder extends Seeder
 {
-
     /**
-     * Auto generated seed file
-     *
-     * @return void
+     * Run the database seeds.
      */
-   public function run(): void
-{
-    DB::table('users')->insert([
-        [
-            'id' => 1,
-            'nama_lengkap' => 'Andika Dwi Putra',
-            'email' => 'dwiputdika@gmail.com',
-            'nomor_pegawai' => '23112021',
-            'role' => 'super_user',
-            'password' => '$2y$12$24dyBXpDKc0buZaC7WtU6u6GC5ZjphCUP5kPKvCA7N014vLGoGGha',
-            'created_at' => '2026-09-03 00:13:52',
-            'updated_at' => '2026-09-07 18:33:45',
-        ],
-        // Masukkan data user lainnya dengan format yang sama jika ingin manual
-    ]);
-}
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'nama_lengkap' => 'Andika Dwi Putra',
+                'email' => 'dwiputdika@gmail.com',
+                'nomor_pegawai' => '23112021',
+                'role' => 'super_user',
+                'email_verified_at' => null,
+                'password' => '$2y$12$24dyBXpDKc0buZaC7WtU6u6GC5ZjphCUP5kPKvCA7N014vLGoGGha',
+                'remember_token' => null,
+                'created_at' => '2026-09-03 00:13:52',
+                'updated_at' => '2026-09-07 18:33:45',
+            ],
+            [
+                'id' => 2,
+                'nama_lengkap' => 'Dhimas Zaky Abiyyu',
+                'email' => 'dhimaszaky102005@gmail.com',
+                'nomor_pegawai' => '23112018',
+                'role' => 'user',
+                'email_verified_at' => null,
+                'password' => '$2y$12$oGpz.VPskPJC6h98elnN3ee7qZIZl2Cgrw2V0o8CP/e/29BQrtvgC',
+                'remember_token' => null,
+                'created_at' => '2026-09-04 09:40:30',
+                'updated_at' => '2026-09-07 18:38:31',
+            ],
+            [
+                'id' => 3,
+                'nama_lengkap' => 'Ananda Gita April',
+                'email' => 'siipooke@gmail.com',
+                'nomor_pegawai' => '23112017',
+                'role' => 'super_user',
+                'email_verified_at' => null,
+                'password' => '$2y$12$A3o8N4rpvAo1EXhsOmZdvOtfi/mvPrlFE33OTCtH1rvI5wtMzvPj.',
+                'remember_token' => null,
+                'created_at' => '2026-09-06 20:59:14',
+                'updated_at' => '2026-09-06 20:59:14',
+            ],
+            [
+                'id' => 4,
+                'nama_lengkap' => 'M. Suwanda',
+                'email' => 'mebius3105@gmail.com',
+                'nomor_pegawai' => '23111006',
+                'role' => 'user',
+                'email_verified_at' => null,
+                'password' => '$2y$12$rxJR5GflWyTvaq8e5lst2O5UmayMaXykgTNGtxXaBWvTjgfubmJ3C',
+                'remember_token' => null,
+                'created_at' => '2026-09-06 20:59:54',
+                'updated_at' => '2026-09-07 18:38:31',
+            ],
+            [
+                'id' => 5,
+                'nama_lengkap' => 'M Ariffan Hidayah',
+                'email' => 'erikpramana68@gmail.com',
+                'nomor_pegawai' => '23112007',
+                'role' => 'user',
+                'email_verified_at' => null,
+                'password' => '$2y$12$ZIbd10RDcZYZEFISoTBMz.jDnb8UssyhHza1Ye.OEqL5eBqamtdzO',
+                'remember_token' => null,
+                'created_at' => '2026-09-06 21:03:09',
+                'updated_at' => '2026-09-07 18:38:31',
+            ],
+            [
+                'id' => 6,
+                'nama_lengkap' => 'Natasha Romanoff',
+                'email' => 'adingbing11@gmail.com',
+                'nomor_pegawai' => '231120xx',
+                'role' => 'user',
+                'email_verified_at' => null,
+                'password' => '$2y$12$3WPQSivRZFK7OlN8/EaBBuywUGFeoOUp/00GC/bDmhMpeKXoTXyZi',
+                'remember_token' => null,
+                'created_at' => '2026-09-06 23:12:02',
+                'updated_at' => '2026-09-07 18:38:31',
+            ],
+            [
+                'id' => 7,
+                'nama_lengkap' => 'Operator Berita',
+                'email' => 'berita.damkar@gmail.com',
+                'nomor_pegawai' => '23113001',
+                'role' => 'operator',
+                'email_verified_at' => null,
+                'password' => '$2y$12$/FjxJDSVPbjOM.byocQqY.T.cYKP6M39dVUo8Q63FBN/U1nsFafBi',
+                'remember_token' => null,
+                'created_at' => '2026-09-06 23:35:56',
+                'updated_at' => '2026-09-06 23:35:56',
+            ],
+        ]);
+    }
 }
