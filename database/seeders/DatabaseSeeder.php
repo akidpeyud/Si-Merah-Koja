53aae5afcf355d8fce2672a3ae55a14fea3371ca
Dimas Zaky <dhimaszaky102005@gmail.com>
2026-09-24 14:09:43 +0700

    tes log

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,6 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
-use App\Models\User;
-=======
->>>>>>> 7800cb3e9effe44e5ed2c2ab0d8e2c1b18246573
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,11 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-<<<<<<< HEAD
-            UserSeeder::class,
-=======
             UsersTableSeeder::class,
->>>>>>> 7800cb3e9effe44e5ed2c2ab0d8e2c1b18246573
         ]);
     }
 }
